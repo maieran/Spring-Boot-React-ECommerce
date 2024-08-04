a8e810095c61d08c16dac2a176566a5eeb565c74
maieran <apak_mc@hotmail.de>
2024-08-04 10:22:48 +0200

    Backend preparation with sql database, docker setup and software folder structure

---
+++ src/test/java/com/ecommerce/Sportscenter/SportscenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.Sportscenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportscenterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
